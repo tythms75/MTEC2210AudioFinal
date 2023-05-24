9ef6290ce0a605eb1903989c0f32d24844116e7a
tythms75 <tythms21@gmail.com>
2023-05-24 14:06:40 -0400

    added correct project

--- Assets/PlaySound.cs
+++ Assets/PlaySound.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI; //Include UI library
 
 public class PlaySound : MonoBehaviour
 {
-
+    //References to variables
     public AudioSource soundPlayer;
+    public Slider volSlider;
+    public Slider pitchSlider;
+    public Toggle muteToggle;
 
 
     // Start is called before the first frame update
@@ -17,7 +21,10 @@ public class PlaySound : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        soundPlayer.volume = volSlider.value;
+        soundPlayer.pitch = pitchSlider.value;
+        soundPlayer.mute = muteToggle.isOn;
+
     }
 
 
